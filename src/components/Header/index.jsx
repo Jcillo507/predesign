3215d28728540267288b2d4e48d8a4b30ba5f67d
Joe Cillo <JCillo507@yahoo.com>
2020-02-17 21:51:29 -0500

    1st comt

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = ()=>{
+  return(
+    <div className='header-ctr'>
+      <div className='header-left'>
+      Logo
+      </div>
+        <div>
+          <a>Projects</a>
+          <a>Skills</a>
+          <a>Contact</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills =()=>{
+  return(
+    <div>
+dwv
+    </div>
+  )
+}
+
+export default Skills
