b8bce20c95a7f7825e3c5dfda2a9a3ae2bde8267
Joe Cillo <JCillo507@yahoo.com>
2020-02-19 19:44:32 -0500

    form styled'

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import './contact.scss'
+import "./contact.scss";
 
 const Contact = () => {
   return (
-    <div className="contact-form" id='contact'>
-      <h2 className='contact-title'>Contact Me</h2>
+    <div className="contact-form" id="contact">
+      <h2 className="contact-title">Contact Me</h2>
+      <h3 className="contact-text">Have a question or want to work with me?</h3>
       <h3 className="contact-text">
-        Have a question or want to work with me?<br></br>
         Send me an e-mail and I will get back to you as fast as I can.
       </h3>
-      {/* <ReactContactForm className='email' to="jcillo507@yahoo.com" buttonText='Submit' /> */}
-
-      <form action="https://formspree.io/jcillo507@yahoo.com" method="POST">
-        <label>
-          Enter your e-mail address:<br></br>
+      <form
+        className="form-ctr"
+        action="https://formspree.io/jcillo507@yahoo.com"
+        method="POST"
+      >
+        <label className="form-label">
+          Enter your e-mail address:
+          <br />
           <input className="form-name" type="text" name="name" />
         </label>
-        <br></br>
-        <label>
-          Enter your message:<br></br>
+        <br />
+        <br />
+        <label className="form-label">
+          Enter your message:
+          <br />
           <textarea className="form-body" type="text" name="body" />
         </label>
-        <br></br>
+        <br />
         <input className="form-submit" type="submit" name="submit" />
       </form>
     </div>
   );
 };
-export default Contact
+export default Contact;
