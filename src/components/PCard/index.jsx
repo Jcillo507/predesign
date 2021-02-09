377903b18172b5eede1fffec66c3994efb37b63b
Joe Cillo <JCillo507@yahoo.com>
2021-02-09 15:17:40 -0500

    image hover done

--- src/components/PCard/index.jsx
+++ src/components/PCard/index.jsx
@@ -5,13 +5,15 @@ const PCard = (props) => {
   return (
     <div className="project">
       <Fade bottom>
-      <img src={props.image} />
-      <p className="project__tech"> {props.tech}</p>
-      <p className="project__name">{props.name}</p>
-      <p className="project__desc">{props.desc}</p>
-      <a href={props.address} target="_blank">
-        <div className="project__link">Visit</div>
-      </a>
+        <div className="project__overlay">
+          <img src={props.image} />
+        </div>
+        <p className="project__tech"> {props.tech}</p>
+        <p className="project__name">{props.name}</p>
+        <p className="project__desc">{props.desc}</p>
+        <a href={props.address} target="_blank">
+          <div className="project__link">Visit</div>
+        </a>
       </Fade>
     </div>
   );
