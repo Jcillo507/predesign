405e6475317ffa219e38631b1d4b38fa419dac4b
Joe Cillo <JCillo507@yahoo.com>
2021-02-10 21:27:47 -0500

    deployed

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -6,9 +6,6 @@ const Info = () => {
   return (
     <section className="info">
       <Fade left>
-        <img src={InfoImage} alt="info imagess" className="info__image" />
-      </Fade>
-      <Fade right>
         <blockquote className="info__text-ctr">
           <h2 className="info__title">01. WHO</h2>
           <h3 className="info__heading">
@@ -26,6 +23,9 @@ const Info = () => {
             achieve results to create the best product.
           </p>
         </blockquote>
+      </Fade>
+      <Fade right>
+        <img src={InfoImage} alt="info imagess" className="info__image" />
       </Fade>
     </section>
   );
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -12,17 +12,6 @@ const Skills = () => {
   return (
     <section className="skills">
       <Fade left>
-        <div className="skills__title-ctr">
-          <h2 className="skills__title">02. HOW</h2>
-          <p className="skills__subtitle">I Specialize in:</p>
-          <ul>
-            <li>Web Development</li>
-            <li>Creative Web Design</li>
-            <li>Mobile App Development</li>
-          </ul>
-        </div>
-      </Fade>
-      <Fade right>
         <div className="skills__card-ctr">
           <img src={ReactLogo} alt="react logo" />
           <img src={SassLogo} alt="sass logo" />
@@ -30,6 +19,17 @@ const Skills = () => {
           <img src={CssLogo} alt="css logo" />
           <img src={HtmlLogo} alt="html logo" />
           <img src={SwiftLogo} alt="swift logo" />
+        </div>
+      </Fade>
+      <Fade right>
+        <div className="skills__title-ctr">
+          <h2 className="skills__title">02. HOW</h2>
+          <p className="skills__subtitle">I am an expert in:</p>
+          <ul>
+            <li>Web Development</li>
+            <li>Creative Web Design</li>
+            <li>Mobile App Development</li>
+          </ul>
         </div>
       </Fade>
     </section>
